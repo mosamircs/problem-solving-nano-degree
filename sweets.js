3f8640a5d2f53b37765d958e18ff0e3391dc4934
Mohamed Samir <mosamircs@gmail.com>
2021-09-12 11:53:58 +0200

    solved problem sweets in nano degree using js

---
+++ sweets.js
@@ -0,0 +1,6 @@
+var flavor = "chocolate";
+var vessel = "cone";
+var toppings = "sprinkles";
+if((flavor==="chocolate"||flavor==="vanilla")&&(vessel==="cone"||vessel=="bowl")&&(toppings==="sprinkles"||toppings==="peanuts")){
+    console.log("I'd like two scoops of "+flavor+" ice cream in a "+vessel+" with "+toppings+".");
+}
